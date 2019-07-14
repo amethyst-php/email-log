ef4a0e31c7d97ba10158cedfcea62cac7da2bf85
railken <railken@protonmail.ch>
2019-07-14 13:08:19 +0200

    fix style

--- src/Managers/EmailLogManager.php
+++ src/Managers/EmailLogManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\EmailLog newEntity()
- * @method \Amethyst\Schemas\EmailLogSchema getSchema()
+ * @method \Amethyst\Models\EmailLog                 newEntity()
+ * @method \Amethyst\Schemas\EmailLogSchema          getSchema()
  * @method \Amethyst\Repositories\EmailLogRepository getRepository()
- * @method \Amethyst\Serializers\EmailLogSerializer getSerializer()
- * @method \Amethyst\Validators\EmailLogValidator getValidator()
- * @method \Amethyst\Authorizers\EmailLogAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\EmailLogSerializer  getSerializer()
+ * @method \Amethyst\Validators\EmailLogValidator    getValidator()
+ * @method \Amethyst\Authorizers\EmailLogAuthorizer  getAuthorizer()
  */
 class EmailLogManager extends Manager
 {
