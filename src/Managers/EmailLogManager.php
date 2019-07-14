fc8f79824e0ddbfeb430bef74373933cc38c0b05
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/EmailLogManager.php
+++ src/Managers/EmailLogManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\EmailLog newEntity()
+ * @method \Amethyst\Schemas\EmailLogSchema getSchema()
+ * @method \Amethyst\Repositories\EmailLogRepository getRepository()
+ * @method \Amethyst\Serializers\EmailLogSerializer getSerializer()
+ * @method \Amethyst\Validators\EmailLogValidator getValidator()
+ * @method \Amethyst\Authorizers\EmailLogAuthorizer getAuthorizer()
+ */
 class EmailLogManager extends Manager
 {
     use ConfigurableManager;
